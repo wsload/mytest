b4de5db068afee0808e3fa7920338604ac6ec2fd
wsload <wsload@163.com>
2017-10-23 02:33:47 +0800

    修改

--- src/com/imooc/first/mytest.java
+++ src/com/imooc/first/mytest.java
@@ -20,7 +20,7 @@ public class mytest {
 		Date date=new Date();
 		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		
-		System.out.println(sdf.format(date)x);
+		System.out.println(sdf.format(date));
 		
 	}
 }
